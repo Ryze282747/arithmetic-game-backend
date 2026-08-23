bed872b469d291603f223fee02ee3e87453cb05e
Zylle <zyllemojica0123@gmail.com>
2026-08-23 21:51:45 +0800

    update game services

--- app/game/services.py
+++ app/game/services.py
@@ -2,6 +2,7 @@ from .validators import validate_game_data
 from .generators import generate_questions
 from app.models.quiz import Quiz
 from app.extensions import db
+from flask import session
 
 def create_new_quiz(data):
   
@@ -9,6 +10,10 @@ def create_new_quiz(data):
   
   if not valid:
     return {"error":error}, 400
+  
+  if session.get("quiz", None):
+    delete_quiz(session["quiz"])
+    session.pop("quiz")
   
   quiz = Quiz.from_obj(data)
   
@@ -19,4 +24,15 @@ def create_new_quiz(data):
   
   db.session.commit()
   
+  session["quiz"] = quiz.id
+  
   return {"questions":questions}, 200
+
+def delete_quiz(quiz_id):
+  quiz = Quiz.query.get(quiz_id)
+  
+  if not quiz:
+    return None
+  
+  db.session.delete(quiz)
+  db.session.commit()
