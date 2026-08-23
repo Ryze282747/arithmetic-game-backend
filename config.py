a16279291f5e24763325a621ec0df39002337e2a
Zylle <zyllemojica0123@gmail.com>
2026-08-23 19:18:52 +0800

    fix app configs

--- config.py
+++ config.py
@@ -5,6 +5,15 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    if os.getenv("DB_SSL", "false").lower() == "true":
+      SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+          "ssl": {
+            "ca": os.path.join(os.path.dirname(__file__),"ca.pem")
+          }
+        }
+      }
+    
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 30
     SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE", "Lax")
     SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", "false") == "true"
